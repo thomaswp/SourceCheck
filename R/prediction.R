ec8815385c46e405b499f5a74d85e3fc45330914
thomaswp <thomaswprice@msn.com>
2016-02-04 17:35:29 -0500

    Prediction analysis

--- R/prediction.R
+++ R/prediction.R
@@ -1,10 +1,13 @@
 library("plyr")
 library("ggplot2")
 
+se <- function(x) sqrt(var(x)/length(x))
+
 prediction <- read.csv("../data/csc200/fall2015/anlysis/guess1Lab/prediction.csv")
 prediction$percActions = prediction$predicted / prediction$actions
 prediction$percHints = prediction$predicted / prediction$hints
 
 combined <<- ddply(prediction, .(policy), summarize, 
                    actionsMean = mean(percActions), actionsSE = se(percActions), 
-                   hintsMean = mean(percSteps), hintsSE = se(percSteps))
+                   hintsMean = mean(percHints), hintsSE = se(percHints),
+                   corActions = cor(percActions, grade), corHints = cor(percHints, grade))
