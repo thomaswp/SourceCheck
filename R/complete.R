7935fca12f535c5fca8e66985518fb6b9f78f038
thomaswp <thomaswprice@msn.com>
2016-02-05 11:46:16 -0500

    Shinier analysis

--- R/complete.R
+++ R/complete.R
@@ -4,6 +4,7 @@ library("ggplot2")
 se <- function(x) sqrt(var(x)/length(x))
 
 loadData <- function() {
+  rm(list=ls())
   maxTest <<- 8;
   complete <<- read.csv("../data/csc200/fall2015/anlysis/guess1Lab/complete.csv")
   tests <<- sapply(0:maxTest, function(i) paste("test", i, sep=""))
@@ -15,6 +16,14 @@ loadData <- function() {
                      hashCount = length(unique(hash)))
 }
 
+idealSolutions <- function() {
+  solutions <- complete[complete$grade==1,c("policy", "hash")]
+  policies <- sapply(unique(complete$policy), as.character)
+  plot(solutions)
+  sapply(policies, function (policy) {
+    length(unique(complete[complete$policy==policy,]$hash))
+  })
+}
 
 plotHash <- function() {
   ggplot(combined, aes(x=slice, y=hashCount, colour=policy)) + 
--- R/grades.R
+++ R/grades.R
@@ -1,6 +1,9 @@
-
+library("plyr")
+library("ggplot2")
+library("reshape2")
 
 loadData <- function() {
+  rm(list=ls())
   
   maxTest <<- 8;
   
@@ -8,21 +11,33 @@ loadData <- function() {
   percs <<- sapply(0:maxTest, function(i) paste("perc", i, sep=""))
     
   grade <<- read.csv("../data/csc200/fall2015/anlysis/guess1Lab/grade.csv")
+  
+  chain <<- read.csv("../data/csc200/fall2015/anlysis/guess1Lab/chain.csv")
+  chain <<- rbind(grade[grade$policy=="Hint All" | grade$policy=="Hint Exemplar",], chain)
+  
   for (i in 1:(maxTest+1)) {
     grade[,percs[i]] <<- grade[,tests[i]] / grade$total
   }
   
-  chain <<- read.csv("../data/csc200/fall2015/anlysis/guess1Lab/chain.csv")
-  chain <<- rbind(grade[grade$policy=="Hint All" | grade$policy=="Hint Exemplar",], chain)
   for (i in 1:(maxTest+1)) {
     chain[,percs[i]] <<- chain[,tests[i]] / chain$total
   }
   
-  students <<- sapply(unique(grades$student), as.character)
+  students <<- sapply(unique(grade$student), as.character)
 }
 
 policies <- function(grades) {
   sapply(unique(grades$policy), as.character)
+}
+
+compareBoth <- function(grades) {
+  grades <- grades[,c("policy", "action", percs)]
+  grades <- melt(grades, id=c("policy", "action"))
+  combined <- ddply(grades, .(policy, action, variable), summarize, mean=mean(sign(value)))
+  ggplot(combined, aes(policy, mean, fill=variable)) +
+    #geom_bar(aes(fill = action), position = "dodge", stat="identity") +
+    geom_bar(stat='identity') +
+    facet_grid(~ action) + scale_fill_grey()
 }
 
 compare <- function(grades, action) {
