cde430a82e30a7f42efaa1e9e239f2910a711340
Thomas P <thomaswprice@msn.com>
2016-02-06 14:30:31 -0500

    Sad distance analysis

--- R/prediction.R
+++ R/prediction.R
@@ -22,17 +22,29 @@ loadData <- function() {
   distance$mNodeDis <<- distance$nodeDis / distance$totalAction
   distance$mHintDis <<- distance$hintDis / distance$totalAction
   distance$percCloser <<- distance$closer / distance$totalHints
+  distance$percFarther <<- distance$farther / distance$totalHints
   
   distanceSummary <<- ddply(distance, .(target, normalized, policy), summarize, 
                             percCloserMean = mean(percCloser), percCloserSD = sd(percCloser),
+                            percFartherMean = mean(percFarther), percFartherSD = sd(percFarther),
                             mNodeDisMean = mean(mNodeDis), mNodeDisSD = sd(mNodeDis),
                             mHintDisMean = mean(mHintDis), mHintDisSD = sd(mHintDis),
                             corCloser = cor(percCloser, grade), corMHintDis = cor(mHintDis, grade))
 }
 
-improvement <- function() {
+improvement <- function(a, b) {
   mean(sapply(students, function(student) {
     d <- distance[distance$student==student & distance$target=="final" & distance$normalized == TRUE,]
-    mean(d[d$policy=="Hint Exemplar",]$percCloser) > mean(d[d$policy=="Direct Ideal",]$percCloser)
+    mean(d[d$policy==a,]$percCloser) > mean(d[d$policy==b,]$percCloser)
   }))
+}
+
+plotCloser <- function() {
+  d <- distance[distance$target=="final" & distance$normalized == TRUE,]
+  ggplot(d, aes(x = percCloser, color=policy)) + geom_density()
+}
+
+plotFarther <- function() {
+  d <- distance[distance$target=="final" & distance$normalized == TRUE,]
+  ggplot(d, aes(x = percFarther, color=policy)) + geom_density()
 }
