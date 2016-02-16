9eba4dbc879606429e33c5cf17554c31f8d7c076
Thomas P <thomaswprice@msn.com>
2016-02-16 11:35:03 -0800

    Plotting deletions; getting confused

--- R/complete.R
+++ R/complete.R
@@ -8,14 +8,22 @@ loadData <- function() {
   rm(list=ls())
   maxTest <<- 8;
   complete <<- read.csv("../data/csc200/fall2015/anlysis/guess1Lab/complete.csv")
+  complete$policy <<- factor(complete$policy, levels = c("Hint All", "Hint Exemplar", "Direct Ideal", "Direct Student"))
   tests <<- sapply(0:maxTest, function(i) paste("test", i, sep=""))
   complete$grade <<- rowSums(complete[,tests]) / 9
   combined <<- ddply(complete, .(policy, slice), summarize, 
                      gradeMean = mean(grade), gradeSE = se(grade), perfect=mean(grade==1),
                      stepsMean = mean(steps), stepsSE = se(steps), 
+                     deletionsMean = mean(deletions), deletionsSE = se(deletions),
                      studentStepsMean = mean(studentSteps), studentStepsSE = se(studentSteps), 
                      hashCount = length(unique(hash)))
   twoColors <<- c("#a1d99b","#2c7fb8")
+  
+  completeAll <<- complete
+  combinedAll <<- combined
+  
+  complete <<- complete[complete$policy == "Hint All" | complete$policy == "Hint Exemplar",]
+  combined <<- combined[combined$policy == "Hint All" | combined$policy == "Hint Exemplar",]
 }
 
 idealSolutions <- function(minGrade) {
@@ -48,6 +56,20 @@ plotGrade <- function() {
     scale_fill_manual(values=twoColors) +
     scale_color_manual(values=twoColors, labels=c("NA", "NE")) +
     scale_y_continuous(limits=c(0.85, 1), label=percent)
+}
+
+plotDeletions <- function() {
+  ggplot(combinedAll, aes(x=slice+1, y=deletionsMean, color=policy)) + 
+    labs(title="Final Solution Grade", x="Slice", y="Grade", color="Policy") +
+    geom_ribbon(aes(x=slice+1, ymin=deletionsMean-deletionsSE, ymax=deletionsMean+deletionsSE, fill=policy), color=NA, alpha=.3) +
+    geom_line() +
+    guides(fill=FALSE) +
+    geom_point() +
+    theme_bw() + 
+    #scale_x_continuous(limits = c(40, 50)) + 
+    #scale_fill_brewer() +
+    #scale_color_brewer() + #, labels=c("NA", "NE", "DI", "DS")) +
+    scale_y_continuous()
 }
 
 plotPerfect <- function() {
