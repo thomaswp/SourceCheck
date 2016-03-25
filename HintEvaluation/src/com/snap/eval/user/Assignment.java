9680b0675fc89f3e2388e5457fa62e836699c36f
thomaswp <thomaswprice@msn.com>
2016-03-25 15:44:08 -0400

    Updated assignments to use 0-based dates

--- HintEvaluation/src/com/snap/eval/user/Assignment.java
+++ HintEvaluation/src/com/snap/eval/user/Assignment.java
@@ -28,21 +28,21 @@ public class Assignment {
 	}
 	
 	public static class Spring2016 {
-		private final static Date start = new GregorianCalendar(2016, 1, 1).getTime();
+		private final static Date start = new GregorianCalendar(2016, 0, 1).getTime();
 		private final static String dataDir = "../data/csc200/spring2016";
 		
 		public final static Assignment LightsCameraAction = new Assignment(dataDir, 
-				"lightsCameraActionHW", start, new GregorianCalendar(2015, 1, 29).getTime());
+				"lightsCameraActionHW", start, new GregorianCalendar(2016, 0, 29).getTime());
 		public final static Assignment PolygonMaker = new Assignment(dataDir, 
-				"polygonMakerLab", start, new GregorianCalendar(2016, 2, 2).getTime());
+				"polygonMakerLab", start, new GregorianCalendar(2016, 1, 2).getTime());
 		public final static Assignment Squiral = new Assignment(dataDir, 
-				"squiralHW", start, new GregorianCalendar(2016, 2, 9).getTime());
+				"squiralHW", start, new GregorianCalendar(2016, 1, 9).getTime());
 		public final static Assignment GuessingGame1 = new Assignment(dataDir, 
-				"guess1Lab", start, new GregorianCalendar(2016, 2, 9).getTime());
+				"guess1Lab", start, new GregorianCalendar(2016, 1, 9).getTime());
 		public final static Assignment GuessingGame2 = new Assignment(dataDir, 
-				"guess2HW", start, new GregorianCalendar(2016, 2, 16).getTime());
+				"guess2HW", start, new GregorianCalendar(2016, 1, 16).getTime());
 		public final static Assignment GuessingGame3 = new Assignment(dataDir, 
-				"guess3Lab", start, new GregorianCalendar(2016, 2, 23).getTime());
+				"guess3Lab", start, new GregorianCalendar(2016, 1, 23).getTime());
 		
 		public final static Assignment[] All = {
 			LightsCameraAction, PolygonMaker, Squiral,
