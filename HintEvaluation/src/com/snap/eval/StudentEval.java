45116aed5e3d5cc561263e9370437ef723ddda44
Thomas P <thomaswprice@msn.com>
2016-03-28 16:57:31 -0700

    Added some grade analyses

--- HintEvaluation/src/com/snap/eval/StudentEval.java
+++ HintEvaluation/src/com/snap/eval/StudentEval.java
@@ -38,6 +38,10 @@ public class StudentEval {
 			
 			int pass0 = 0;
 			
+			double totalGrade = 0;
+			int perfectGrade = 0;
+			double minGrade = 1;
+			
 			for (String student : paths.keySet()) {
 				if (skip-- > 0) {
 					continue;
@@ -56,7 +60,11 @@ public class StudentEval {
 				Node solution = nodes.get(nodes.size() - 1);
 				if (AutoGrader.graders[0].pass(solution)) {
 					pass0++;
-				}
+				}				
+				double grade = AutoGrader.numberGrade(solution);
+				totalGrade += grade;
+				minGrade = Math.min(grade, minGrade);
+				if (grade == 1) perfectGrade++;
 				
 				for (Node node : nodes) {
 					String ns = node.toCanonicalString();
@@ -68,12 +76,16 @@ public class StudentEval {
 				totalNoDouble += studentSet.size();
 				solutions.add(nodes.get(nodes.size() - 1).toCanonicalString());
 				students++;
+
 			}
 			
 			System.out.println("Pass0: " + pass0 + "/" + students);
 			System.out.println("Unique: " + seen.size() + "/" + total);
 			System.out.println("Unique no double: " + seen.size()  + "/" +  totalNoDouble);
 			System.out.println("Unique solutions: " + solutions.size()  + "/" +  students);
+			System.out.printf("Mean grade: %.03f\n", (totalGrade / students));
+			System.out.println("Perfect grades: " + perfectGrade);
+			System.out.println("Min grade: " + minGrade);
 		}
 	}
 }
