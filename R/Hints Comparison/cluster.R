2f65344ad729a73782d82e391db5b1c58bbdf928
thomaswp <thomaswprice@msn.com>
2016-11-03 16:07:20 -0400

    Added metaMDS

--- R/Hints Comparison/cluster.R
+++ R/Hints Comparison/cluster.R
@@ -15,6 +15,11 @@ states <- states[order(states$id),]
 
 states$x <- t[,1]
 states$y <- t[,2]
-states$g <- substr(states$attempt, 1, 2)
+states$g <- substr(states$attempt, 1, 1)
 
-ggplot(states, aes(x, y)) + geom_point(aes(shape=submitted, color=attempt)) + geom_line(aes(group=attempt), alpha=0.3) + scale_color_discrete()
+ggplot(states, aes(x, y)) + geom_point(aes(shape=submitted, color=g)) + geom_line(aes(color=g, group=attempt), alpha=0.3) + scale_color_discrete()
+
+
+mds <- metaMDS(x)
+states$x <- mds$points[,1]
+states$y <- mds$points[,2]
