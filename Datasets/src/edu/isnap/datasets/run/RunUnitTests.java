b3b52d1eb9d1ff5b10b58a0c74ddff4227ae5b7c
thomaswp <thomaswprice@msn.com>
2016-11-13 21:18:19 -0500

    Added initial brickWall unit tests

--- Datasets/src/edu/isnap/datasets/run/RunUnitTests.java
+++ Datasets/src/edu/isnap/datasets/run/RunUnitTests.java
@@ -1,10 +1,10 @@
 package edu.isnap.datasets.run;
 
-import edu.isnap.datasets.Spring2016;
+import edu.isnap.datasets.HelpSeeking;
 import edu.isnap.unittest.TestRunner;
 
 public class RunUnitTests {
 	public static void main(String[] args) {
-		TestRunner.run(Spring2016.All);
+		TestRunner.run(HelpSeeking.All);
 	}
 }
