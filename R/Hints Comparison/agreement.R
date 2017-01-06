079fbe33e6c7955f2436d7dd341ca96861aaab68
thomaswp <thomaswprice@msn.com>
2017-01-06 14:05:21 -0500

    Updated agreement code

--- R/Hints Comparison/agreement.R
+++ R/Hints Comparison/agreement.R
@@ -11,6 +11,11 @@ agreement <- function(x, y) {
 testAgreement <- function() {
   r1 <- c.merge(read.csv("data/firstHints1.csv"), read.csv("data/secondHints1.csv"))
   r2 <- c.merge(read.csv("data/firstHints2.csv"), read.csv("data/secondHints2.csv"))
-  
+
+  # 0.863  
   agreement(r1$Relevance, r2$Relevance)
+  # 0.769
+  agreement(r1$Correctness, r2$Correctness)
+  # 0.696
+  agreement(r1$Interpretability, r2$Interpretability)
 }
