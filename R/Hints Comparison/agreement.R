7b179a13f5728c8d5c7a6b63539cbf0fbae5d53d
thomaswp <thomaswprice@msn.com>
2017-01-08 10:34:23 -0500

    Changed hint agreement to use only used hints

--- R/Hints Comparison/agreement.R
+++ R/Hints Comparison/agreement.R
@@ -12,10 +12,14 @@ testAgreement <- function() {
   r1 <- c.merge(read.csv("data/firstHints1.csv"), read.csv("data/secondHints1.csv"))
   r2 <- c.merge(read.csv("data/firstHints2.csv"), read.csv("data/secondHints2.csv"))
 
-  # 0.863  
+  usedHints <- c(chances$firstHint, chances$secondHint)
+  r1 <- r1[r1$Row.ID %in% usedHints,]
+  r2 <- r2[r2$Row.ID %in% usedHints,]
+  
+  # 0.840
   agreement(r1$Relevance, r2$Relevance)
-  # 0.769
+  # 0.729
   agreement(r1$Correctness, r2$Correctness)
-  # 0.696
+  # 0.637
   agreement(r1$Interpretability, r2$Interpretability)
 }
