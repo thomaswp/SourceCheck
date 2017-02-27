a4c3d308cd08f2902397b8f53985afa34afde5ae
Thomas P <thomaswprice@msn.com>
2017-02-27 12:31:10 -0500

    Differentiated plotAll and plotIdeal

--- R/Agreement/agreement.R
+++ R/Agreement/agreement.R
@@ -6,8 +6,13 @@ loadData <- function() {
   squiral <- read_csv("../../data/csc200/fall2016/analysis/squiralHW/agreement.csv")
 }
 
-plotStats <- function(data) {
-  ggplot(data[data$actual == "all" & data$edit != "keep",], aes(x=edit, y=value)) + 
+plotAll <- function(data) {
+  ggplot(data[data$actual == "all" & data$edit != "keep" & data$pred != "highlight-sed",], aes(x=edit, y=value)) + 
     geom_bar(aes(fill=pred), position="dodge", stat="identity") + 
     facet_grid(. ~ stat)
+}
+
+plotIdeal <- function(data) {
+  ggplot(data[data$actual == "all" & data$edit != "keep" & data$pred != "highlight-sed" & data$stat=="recall",], aes(x=edit, y=value)) + 
+    geom_bar(aes(fill=pred), position="dodge", stat="identity")
 }
