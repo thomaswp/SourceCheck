45c91228cd235d670cfda4811af400e36df1a998
Thomas P <thomaswprice@msn.com>
2017-04-17 15:58:11 -0400

    Parsed submitted for Spring2017

--- Datasets/src/edu/isnap/datasets/Spring2017.java
+++ Datasets/src/edu/isnap/datasets/Spring2017.java
@@ -12,8 +12,18 @@ public class Spring2017 extends Dataset {
 	public final static String dataFile = dataDir + ".csv";
 	public final static Spring2017 instance = new Spring2017();
 
+	// TODO: resolve 5 missing assignments
 	public final static Assignment LightsCameraAction = new Assignment(instance,
 			"lightsCameraActionHW", Assignment.date(2017, 2, 2), true) {
+
+		@Override
+		public Assignment getLocationAssignment(String attemptID) {
+			switch (attemptID) {
+				// Did work under Project for some reason
+				case "f6981651-60e8-4212-8c58-889e85e12c9f": return Project;
+			}
+			return super.getLocationAssignment(attemptID);
+		}
 	};
 
 	public final static Assignment PolygonMaker = new Assignment(instance,
@@ -22,19 +32,58 @@ public class Spring2017 extends Dataset {
 
 	public final static Assignment Squiral = new Assignment(instance,
 			"squiralHW", Assignment.date(2017, 2, 9), true, false, null) {
+
+		@Override
+		public Assignment getLocationAssignment(String attemptID) {
+			switch (attemptID) {
+				// Did work under the wrong assignment
+				case "c87035d6-f406-4598-8a36-0cb92e482650": return PolygonMaker;
+			}
+			return super.getLocationAssignment(attemptID);
+		};
+
+		@Override
+		public boolean ignore(String attemptID) {
+			// Only one short viewing log under None, but no work
+			return "d0393743-2646-40c6-92e9-1897927e1f82".equals(attemptID);
+		};
 	};
 
 	public final static Assignment GuessingGame1 = new Assignment(instance,
 			"guess1Lab", Assignment.date(2017, 2, 9), true, false, null) {
+
+		@Override
+		public Assignment getLocationAssignment(String attemptID) {
+			switch (attemptID) {
+				// Did work under the wrong assignment
+				case "73adfec1-3666-4499-96e6-5737e13d7806": return GuessingGame2;
+			}
+			return super.getLocationAssignment(attemptID);
+		};
 	};
 
 	public final static Assignment GuessingGame2 = new Assignment(instance,
 			"guess2HW", Assignment.date(2017, 2, 16), true, false, GuessingGame1) {
+
+		@Override
+		public boolean ignore(String attemptID) {
+			// Missing most logs for some reason
+			return "bba76353-ad96-489e-9e62-c264fedd03cd".equals(attemptID);
+		};
 	};
 
 
 	public final static Assignment GuessingGame3 = new Assignment(instance,
 			"guess3Lab", Assignment.date(2017, 2, 23), true, false, null) {
+
+		@Override
+		public boolean ignore(String attemptID) {
+			return "81a8b07f-4f2c-4cd0-a84d-137c90c7c6b2".equals(attemptID);
+		};
+	};
+
+	public final static Assignment Project = new Assignment(instance,
+			"project", null, true, false, null) {
 	};
 
 	public final static Assignment[] All = {
--- Datasets/src/edu/isnap/datasets/run/RunParseSubmitted.java
+++ Datasets/src/edu/isnap/datasets/run/RunParseSubmitted.java
@@ -2,15 +2,16 @@ package edu.isnap.datasets.run;
 
 import java.io.IOException;
 
-import edu.isnap.datasets.Fall2015;
+import edu.isnap.dataset.Assignment;
+import edu.isnap.datasets.Spring2017;
 import edu.isnap.parser.ParseSubmitted;
 
 public class RunParseSubmitted {
 	public static void main(String[] args) throws IOException {
-//		for (Assignment assignment : Fall2016.All) {
-//			ParseSubmitted.parseSubmitted(assignment);
-//		}
-		ParseSubmitted.parseSubmitted(Fall2015.Squiral);
+		for (Assignment assignment : Spring2017.All) {
+			ParseSubmitted.parseSubmitted(assignment);
+		}
+//		ParseSubmitted.parseSubmitted(Spring2017.GuessingGame3);
 //		ParseSubmitted.printToGrade(Fall2015.GuessingGame1);
 	}
 }
