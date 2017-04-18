e957cd552d5c3562696381463939ac503398623e
Thomas P <thomaswprice@msn.com>
2017-04-18 11:23:36 -0400

    Verified historical (unlogged) hints didn't change with hintEval merge

--- Datasets/src/edu/isnap/datasets/run/RunHintDataRepairer.java
+++ Datasets/src/edu/isnap/datasets/run/RunHintDataRepairer.java
@@ -3,14 +3,15 @@ package edu.isnap.datasets.run;
 import java.io.IOException;
 
 import edu.isnap.dataset.Assignment;
+import edu.isnap.datasets.Fall2016;
 import edu.isnap.datasets.Spring2017;
 import edu.isnap.hint.HighlightDataRepairer;
 
 public class RunHintDataRepairer {
 	public static void main(String[] args) throws IOException {
 		for (Assignment assignment : Spring2017.All) {
-//			HighlightDataRepairer.repair(assignment, Fall2016.instance, 1);
-			HighlightDataRepairer.testRepair(assignment);
+			HighlightDataRepairer.repair(assignment, Fall2016.instance, 1);
+//			HighlightDataRepairer.testRepair(assignment);
 		}
 	}
 }
