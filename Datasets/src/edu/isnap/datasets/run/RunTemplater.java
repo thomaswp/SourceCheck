1ae0bf4f47eabb3443e81823c4371f4f2c4966e4
Thomas P <thomaswprice@msn.com>
2017-06-22 13:51:14 -0400

    Added U1_L3_L6

--- Datasets/src/edu/isnap/datasets/run/RunTemplater.java
+++ Datasets/src/edu/isnap/datasets/run/RunTemplater.java
@@ -3,16 +3,15 @@ package edu.isnap.datasets.run;
 import java.io.IOException;
 
 import edu.isnap.ctd.graph.Node;
-import edu.isnap.dataset.Assignment;
 import edu.isnap.datasets.BJCSolutions2017;
 import edu.isnap.template.parse.TemplateParser;
 
 public class RunTemplater {
 	public static void main(String[] args) throws IOException {
 		Node.PrettyPrintSpacing = 4;
-//		TemplateParser.parseTemplate(BJCSolutions2017.U2_L4_BrickWall);
-		for (Assignment assignment : BJCSolutions2017.All) {
-			TemplateParser.parseTemplate(assignment);
-		}
+		TemplateParser.parseTemplate(BJCSolutions2017.U1_L3_P6_NestSquares);
+//		for (Assignment assignment : BJCSolutions2017.All) {
+//			TemplateParser.parseTemplate(assignment);
+//		}
 	}
 }
