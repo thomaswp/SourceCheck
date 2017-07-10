234b8fc60e42a307271526932a756d3c473472dd
Thomas P <thomaswprice@msn.com>
2017-07-10 11:36:11 -0400

    Updated U5_L1 to support more solutions

--- Datasets/src/edu/isnap/datasets/run/RunTemplater.java
+++ Datasets/src/edu/isnap/datasets/run/RunTemplater.java
@@ -9,7 +9,7 @@ import edu.isnap.template.parse.TemplateParser;
 public class RunTemplater {
 	public static void main(String[] args) throws IOException {
 		Node.PrettyPrintSpacing = 4;
-		TemplateParser.parseTemplate(BJCSolutions2017.U3_L1_ContactList);
+		TemplateParser.parseTemplate(BJCSolutions2017.U5_L1_Search);
 //		for (Assignment assignment : BJCSolutions2017.All) {
 //			TemplateParser.parseTemplate(assignment);
 //		}
