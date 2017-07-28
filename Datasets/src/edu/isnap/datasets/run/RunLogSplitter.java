bd6beaa5486c05879b7507430d98426828410252
Thomas P <thomaswprice@msn.com>
2017-07-28 14:18:35 -0400

    Fixed merge bug

--- Datasets/src/edu/isnap/datasets/run/RunLogSplitter.java
+++ Datasets/src/edu/isnap/datasets/run/RunLogSplitter.java
@@ -15,6 +15,6 @@ public class RunLogSplitter {
 		// Note: Prior to running this you must *manually* delete the "parsed" directory of the
 		// dataset folder
 		SnapParser.clean(dataset.dataDir);
-		LogSplitter.splitStudentRecords(dataset.dataFile);
+		new LogSplitter().splitStudentRecords(dataset);
 	}
 }
