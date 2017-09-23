02e9a3b36ebb1027998a8df4c2f901005f3e3f58
thomaswp <thomaswprice@msn.com>
2017-09-23 17:42:21 -0400

    Fixed bug with sorting EditHints

--- Datasets/src/edu/isnap/datasets/run/RunTemplater.java
+++ Datasets/src/edu/isnap/datasets/run/RunTemplater.java
@@ -9,7 +9,7 @@ import edu.isnap.template.parse.TemplateParser;
 public class RunTemplater {
 	public static void main(String[] args) throws IOException {
 		Node.PrettyPrintSpacing = 4;
-		TemplateParser.parseTemplate(CSC200Solutions.GuessingGame1);
+		TemplateParser.parseTemplate(CSC200Solutions.Squiral);
 //		for (Assignment assignment : BJCSolutions2017.All) {
 //			TemplateParser.parseTemplate(assignment);
 //		}
