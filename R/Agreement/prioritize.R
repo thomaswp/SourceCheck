a041c1aa1c35e74ceaadb0392e8376a9f4812fab
thomaswp <thomaswprice@msn.com>
2017-10-20 15:21:06 -0400

    Looked into numerator as well

--- R/Agreement/prioritize.R
+++ R/Agreement/prioritize.R
@@ -6,5 +6,6 @@ prioritize <- read_csv("../../data/csc200/spring2017/analysis/edm2017-prioritize
 prioritize$category <- ordered(prioritize$category, c("bad", "ok", "ideal"))
 
 ggplot(prioritize, aes(x=assignment, y=consensus, fill=category)) + geom_boxplot()
+ggplot(prioritize, aes(x=assignment, y=consensusNum, fill=category)) + geom_boxplot()
 ggplot(prioritize, aes(x=assignment, y=creation, fill=category)) + geom_boxplot()
 ggplot(prioritize, aes(x=consensus, y=creation, color=category, shape=assignment)) + geom_point()
