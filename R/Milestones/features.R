9ca3b006b60c99b6c245ad8d0175875b6f16d02a
Thomas P <thomaswprice@msn.com>
2018-04-04 17:38:50 -0400

    Using distance for embed

--- R/Milestones/features.R
+++ R/Milestones/features.R
@@ -7,13 +7,16 @@ library(factoextra)
 disMat <- as.matrix(read_csv("../../data/csc200/all/analysis/squiralHW/feature-jaccard.csv", col_names = FALSE))
 disMat <- 1 - disMat
 
+disMat <- as.matrix(read_csv("../../data/csc200/all/analysis/squiralHW/feature-order.csv", col_names = FALSE))
+disMat <- abs(disMat)
+
 features <- read_csv("../../data/csc200/all/analysis/squiralHW/features.csv", col_names = TRUE)
 
 fviz_nbclust(disMat, pam, "gap")
 
 embed <- tsne(disMat, k=2, max_iter = 5000, epoch=500)
 
-clusters <- pam(disMat, 7)
+clusters <- pam(disMat, 8)
 
 features$x <- embed[,1]
 features$y <- embed[,2]
