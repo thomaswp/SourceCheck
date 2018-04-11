6cb39771eeebc7b028bfe70db6c6112cf152b277
thomaswp <thomaswprice@msn.com>
2018-04-10 23:46:03 -0400

    Calculated stats

--- R/Milestones/compare.R
+++ R/Milestones/compare.R
@@ -45,9 +45,31 @@ equalsMatrix <- function(states) {
 compareMats <- function(statesTut, statesAuto) {
   mat1 <- equalsMatrix(statesAuto)
   mat2 <- equalsMatrix(statesTut)
-  mat3 <- mat1 * 2 + mat3
+  mat3 <- mat1 * 2 + mat2
   ggplot(melt(mat3), aes(x=Var1, y=Var2, color=as.factor(value))) + geom_point() + 
     scale_color_manual(labels=c("TN", "FN", "FP", "TP"), values=c("#ffffff", "#ff0000", "#0000ff", "#ff00ff"))
+}
+
+compareMatsI <- function(auto, tutor, i) {
+  traceID <- unique(auto$traceID)[i]
+  compareMats(tutor$state[tutor$traceID == traceID], auto$state[auto$traceID == traceID])
+}
+
+confMat <- function(statesTut, statesAuto) {
+  mat1 <- equalsMatrix(statesAuto)
+  mat2 <- equalsMatrix(statesTut)
+  mat3 <- (mat1 * 2) + mat2
+  conf <- table(mat3)
+  conf <- conf / sum(conf)
+  names(conf) <- c("TN", "FN", "FP", "TP")
+  frame <- data.frame(tn=conf["TN"], fn=conf["FN"], fp=conf["FP"], tp=conf["TP"])
+  rownames(frame) <- c()
+  frame
+}
+
+confMatI <- function(auto, tutor, i) {
+  traceID <- unique(auto$traceID)[i]
+  confMat(tutor$state[tutor$traceID == traceID], auto$state[auto$traceID == traceID])
 }
 
 run <- function() {
@@ -57,10 +79,23 @@ run <- function() {
   auto <- filterFeatures(auto)
   tutor <- filterFeatures(tutor)
   
+  auto$traceID <- substr(auto$traceID, 1, 8)
+  tutor$traceID <- substr(tutor$traceID, 1, 8)
+  
   auto$state <- getStates(auto)
   tutor$state <- getStates(tutor)
   
+  stats <- data.frame(tn=numeric(0), fn=numeric(0), fp=numeric(0), tp=numeric(0))
   for (traceID in unique(auto$traceID)) {
-    compareMats(auto$state[auto$traceID == traceID])
+    row <- confMat(tutor$state[tutor$traceID == traceID], auto$state[auto$traceID == traceID])
+    stats <- rbind(stats, row)
   }
+  stats$prec <- stats$tp / (stats$tp + stats$fp)
+  stats$rec <- stats$tp / (stats$tp + stats$fn)
+  stats$f1 <- 2 / (1/stats$prec + 1/stats$rec)
+  stats
+  
+  mean(stats$f1)
+  mean(stats$prec)
+  mean(stats$rec)
 }
