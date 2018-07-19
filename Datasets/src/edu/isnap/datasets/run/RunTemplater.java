966e04e44602c183a9321a891fcd6c38eced83f0
thomaswp <thomaswprice@msn.com>
2018-07-18 23:39:19 -0400

    Parse all the assignments!

--- Datasets/src/edu/isnap/datasets/run/RunTemplater.java
+++ Datasets/src/edu/isnap/datasets/run/RunTemplater.java
@@ -3,6 +3,7 @@ package edu.isnap.datasets.run;
 import java.io.IOException;
 
 import edu.isnap.ctd.graph.Node;
+import edu.isnap.dataset.Assignment;
 import edu.isnap.datasets.CampSolutions;
 import edu.isnap.template.parse.TemplateParser;
 
@@ -10,9 +11,8 @@ public class RunTemplater {
 	public static void main(String[] args) throws IOException {
 		Node.PrettyPrintSpacing = 4;
 		Node.PrettyPrintUseColon = true;
-		TemplateParser.parseSnapTemplate(CampSolutions.Practice);
-//		for (Assignment assignment : CSC200Solutions.All) {
-//			TemplateParser.parseSnapTemplate(assignment);
-//		}
+		for (Assignment assignment : CampSolutions.All) {
+			TemplateParser.parseSnapTemplate(assignment);
+		}
 	}
 }
