1bccd0db30f8b7a9196d0774d9627d75ef31ecd9
thomaswp <thomaswprice@msn.com>
2018-08-26 18:17:34 -0400

    Created simple polygon maker assignment for MTurk study

---
+++ Datasets/src/edu/isnap/datasets/MTurkSolutions.java
@@ -0,0 +1,46 @@
+package edu.isnap.datasets;
+
+import java.util.Date;
+
+import edu.isnap.dataset.Assignment;
+import edu.isnap.dataset.Dataset;
+import edu.isnap.hint.ConfigurableAssignment;
+import edu.isnap.hint.SnapHintConfig;
+
+public class MTurkSolutions extends Dataset {
+
+	public final static Date start = null;
+	public final static String dataDir = "../data/mturk/solutions";
+	public final static MTurkSolutions instance = new MTurkSolutions();
+
+	private static class TemplateAssignment extends ConfigurableAssignment {
+
+		public TemplateAssignment(String name) {
+			super(instance, name, null, false);
+		}
+
+		@Override
+		public SnapHintConfig getConfig() {
+			SnapHintConfig config = new SnapHintConfig();
+			config.preprocessSolutions = false;
+			return config;
+		}
+	}
+
+	public final static Assignment PolygonMakerSimple =
+			new TemplateAssignment("polygonMakerSimple");
+
+	public final static Assignment[] All = {
+		PolygonMakerSimple
+	};
+
+	private MTurkSolutions() {
+		super(start, dataDir);
+	}
+
+	@Override
+	public Assignment[] all() {
+		return All;
+	}
+
+}
--- Datasets/src/edu/isnap/datasets/run/RunTemplater.java
+++ Datasets/src/edu/isnap/datasets/run/RunTemplater.java
@@ -3,14 +3,14 @@ package edu.isnap.datasets.run;
 import java.io.IOException;
 
 import edu.isnap.ctd.graph.Node;
-import edu.isnap.datasets.CSC200Solutions;
+import edu.isnap.datasets.MTurkSolutions;
 import edu.isnap.template.parse.TemplateParser;
 
 public class RunTemplater {
 	public static void main(String[] args) throws IOException {
 		Node.PrettyPrintSpacing = 4;
 		Node.PrettyPrintUseColon = true;
-		TemplateParser.parseSnapTemplate(CSC200Solutions.PolygonMaker);
+		TemplateParser.parseSnapTemplate(MTurkSolutions.PolygonMakerSimple);
 //		for (Assignment assignment : CSC200Solutions.All) {
 //			TemplateParser.parseSnapTemplate(assignment);
 //		}
