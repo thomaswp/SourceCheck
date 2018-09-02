986e7d1e4b0930b8410ba9c3cd9227998c8b3bb6
thomaswp <thomaswprice@msn.com>
2018-09-02 18:56:10 -0400

    Added/updated CSC200 hints for polygon/squiral

--- Datasets/src/edu/isnap/datasets/run/RunTemplater.java
+++ Datasets/src/edu/isnap/datasets/run/RunTemplater.java
@@ -3,16 +3,17 @@ package edu.isnap.datasets.run;
 import java.io.IOException;
 
 import edu.isnap.ctd.graph.Node;
-import edu.isnap.datasets.MTurkSolutions;
+import edu.isnap.dataset.Assignment;
+import edu.isnap.datasets.CSC200Solutions;
 import edu.isnap.template.parse.TemplateParser;
 
 public class RunTemplater {
 	public static void main(String[] args) throws IOException {
 		Node.PrettyPrintSpacing = 4;
 		Node.PrettyPrintUseColon = true;
-		TemplateParser.parseSnapTemplate(MTurkSolutions.DrawTriangles);
-//		for (Assignment assignment : CSC200Solutions.All) {
-//			TemplateParser.parseSnapTemplate(assignment);
-//		}
+//		TemplateParser.parseSnapTemplate(CSC200Solutions.Squiral);
+		for (Assignment assignment : CSC200Solutions.All) {
+			TemplateParser.parseSnapTemplate(assignment);
+		}
 	}
 }
