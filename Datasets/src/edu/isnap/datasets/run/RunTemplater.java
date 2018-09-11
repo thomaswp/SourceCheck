462f58a452e5607e50732689adfa497243262752
Samiha Marwan <samarwan>
2018-09-10 21:46:39 -0400

    Updated PongLab1 and Pong2HW, paddle is working but ball no

--- Datasets/src/edu/isnap/datasets/run/RunTemplater.java
+++ Datasets/src/edu/isnap/datasets/run/RunTemplater.java
@@ -10,7 +10,7 @@ public class RunTemplater {
 	public static void main(String[] args) throws IOException {
 		Node.PrettyPrintSpacing = 4;
 		Node.PrettyPrintUseColon = true;
-		TemplateParser.parseSnapTemplate(CSC200Solutions.Pong1Lab);
+		TemplateParser.parseSnapTemplate(CSC200Solutions.Pong2HW);
 //		for (Assignment assignment : CSC200Solutions.All) {
 //			TemplateParser.parseSnapTemplate(assignment);
 //		}
