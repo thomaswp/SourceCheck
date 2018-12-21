5c63e920d70ccbd3b769dbe4811675cb19c65b50
thomaswp <thomaswprice@msn.com>
2018-12-21 18:06:22 -0500

    Parsed submitted for Fall 2018 (with bug; see TODO)

--- Datasets/src/edu/isnap/datasets/Fall2018.java
+++ Datasets/src/edu/isnap/datasets/Fall2018.java
@@ -24,16 +24,43 @@ public class Fall2018 extends Dataset {
 
 	/** Actually a homework */
 	public final static Assignment PolygonMaker = new Assignment(instance,
-			"polygonMakerLab", null, true, false, null) {
+			"polygonMakerLab", null, true, false, Squiral) {
+
+		@Override
+		public Assignment getLocationAssignment(String attemptID) {
+			if ("9e6b2c2e-5feb-4a10-80c9-278071f1cf84".equals(attemptID) ||
+					"b23ea84f-11fe-4191-971f-c14907c06447".equals(attemptID)) {
+				return Squiral;
+			}
+			return super.getLocationAssignment(attemptID);
+		}
+
+		@Override
+		public Integer getSubmittedRow(String attemptID) {
+			if ("2b08b082-8e75-48d9-8b53-cdd9f7d3dc7a".equals(attemptID)) return 122360;
+			return super.getSubmittedRow(attemptID);
+		};
 	};
 
 	public final static Assignment Pong1 = new Assignment(instance,
 			"pong1Lab", null, true, false, null) {
+
+		@Override
+		public Integer getSubmittedRow(String attemptID) {
+			if ("5c24260a-d8b0-474f-b9d4-30d9ffbdb6f2".equals(attemptID)) return 154803;
+			return super.getSubmittedRow(attemptID);
+		};
 	};
 
-	/** Snowstorm disrupted much of data collection */
+	/** Snowstorm disrupted much of data collection: at least 6 submissions not done in iSnap */
 	public final static Assignment Pong2 = new Assignment(instance,
 			"pong2HW", null, true, false, Pong1) {
+
+		// TODO: The project b300be20-adfb-4488-82eb-d5f4f783b6cc has two differnt submissions by
+		// different students. They appear to have different traces, so it's not just cheating
+		// (at least not _just_ cheating). I renamed one to have a '+' at the end, effectively
+		// removing it. When we finally refactor to store attempts by their user id, not project id,
+		// we need to reparse this assignment to include both
 	};
 
 	public final static Assignment GuessingGame1 = new Assignment(instance,
@@ -43,6 +70,11 @@ public class Fall2018 extends Dataset {
 	/** Actually a homework */
 	public final static Assignment GuessingGame2 = new Assignment(instance,
 			"guess2Lab", null, true, false, GuessingGame1) {
+		@Override
+		public Assignment getLocationAssignment(String attemptID) {
+			if ("dec1802b-f3b5-48a3-aec6-61a46f666060".equals(attemptID)) return Pong1;
+			return super.getLocationAssignment(attemptID);
+		};
 	};
 
 	public final static Assignment Project = new Assignment(instance,
--- Datasets/src/edu/isnap/datasets/run/RunParseSubmitted.java
+++ Datasets/src/edu/isnap/datasets/run/RunParseSubmitted.java
@@ -7,10 +7,12 @@ import edu.isnap.parser.ParseSubmitted;
 
 public class RunParseSubmitted {
 	public static void main(String[] args) throws IOException {
+		// NOTE: You may need to use the -Xmx argument to increase max memory for some runs
+
 //		for (Assignment assignment : Spring2017.All) {
 //			ParseSubmitted.parseSubmitted(assignment);
 //		}
-		ParseSubmitted.parseSubmitted(Fall2018.LightsCameraAction);
+		ParseSubmitted.parseSubmitted(Fall2018.GuessingGame2);
 //		ParseSubmitted.printToGrade(Fall2015.GuessingGame1);
 	}
 }
