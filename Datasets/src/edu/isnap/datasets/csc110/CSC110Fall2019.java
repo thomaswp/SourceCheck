b5a3c44e09b1ca5684b982f718b122ac60ed04e3
Thomas Price <twprice@ncsu.edu>
2019-10-07 13:05:52 -0400

    Added initial CSC110 assignmets

--- Datasets/src/edu/isnap/datasets/csc110/CSC110Fall2019.java
+++ Datasets/src/edu/isnap/datasets/csc110/CSC110Fall2019.java
@@ -13,11 +13,67 @@ public class CSC110Fall2019 extends CSC110Dataset {
 	public final static String dataFile = dataDir + ".csv";
 	public final static CSC110Fall2019 instance = new CSC110Fall2019();
 
+	public final static Assignment BrickWall = new ConfigurableAssignment(instance,
+			"brickWall", null, true) {
+	};
 	public final static Assignment DaisyDesign = new ConfigurableAssignment(instance,
 			"daisyDesignHW", null, true) {
 	};
+	public final static Assignment Frogger = new ConfigurableAssignment(instance,
+			"frogger", null, true) {
+	};
+	public final static Assignment GuessingGame1 = new ConfigurableAssignment(instance,
+			"guess1Lab", null, true) {
+	};
+	public final static Assignment GuessingGame = new ConfigurableAssignment(instance,
+			"guessingGame", null, true) {
+	};
+	public final static Assignment GuessingGame2 = new ConfigurableAssignment(instance,
+			"guessingGame2", null, true) {
+	};
+	public final static Assignment Hangman1 = new ConfigurableAssignment(instance,
+			"hangman1", null, true) {
+	};
+	public final static Assignment Hangman3 = new ConfigurableAssignment(instance,
+			"hangman3", null, true) {
+	};
+	public final static Assignment Intro = new ConfigurableAssignment(instance,
+			"intro", null, true) {
+	};
+	public final static Assignment None = new ConfigurableAssignment(instance,
+			"none", null, true) {
+	};
+	public final static Assignment PolygonMakerLab = new ConfigurableAssignment(instance,
+			"polygonMakerLab", null, true) {
+	};
+	public final static Assignment Project1 = new ConfigurableAssignment(instance,
+			"project1", null, true) {
+	};
+	public final static Assignment Squiral = new ConfigurableAssignment(instance,
+			"squiral", null, true) {
+	};
+	public final static Assignment SquiralHW = new ConfigurableAssignment(instance,
+			"squiralHW", null, true) {
+	};
+	public final static Assignment TypingRace = new ConfigurableAssignment(instance,
+			"typingRace", null, true) {
+	};
 	public final static Assignment[] All = {
-		DaisyDesign
+			BrickWall,
+			DaisyDesign,
+//			Frogger,
+			GuessingGame1,
+			GuessingGame,
+			GuessingGame2,
+			Hangman1,
+			Hangman3,
+			Intro,
+			None,
+			PolygonMakerLab,
+			Project1,
+			Squiral,
+			SquiralHW,
+			TypingRace,
 	};
 
 	private CSC110Fall2019() {
