767a8e3acbc4de5fbecb394228a7de864eeb0073
Wengran Wang <wwang33@Wengrans-MacBook-Pro.local>
2019-10-07 13:53:28 -0400

    merged conflict in the file RunParseSubmitted, added gitignore to ignore the .DS_STOREs,  added float2int.py to change (0-100) scale to (0-2) scale, added final grades data brickwall.csv and daisydesignhw.csv

--- Datasets/src/edu/isnap/datasets/run/RunParseSubmitted.java
+++ Datasets/src/edu/isnap/datasets/run/RunParseSubmitted.java
@@ -9,10 +9,13 @@ public class RunParseSubmitted {
 	public static void main(String[] args) throws IOException {
 		// NOTE: You may need to use the -Xmx argument to increase max memory for some runs
 
+
 //		for (Assignment assignment : Spring2017.All) {
 //			ParseSubmitted.parseSubmitted(assignment);
 //		}
 		ParseSubmitted.parseSubmitted(CSC110Fall2019.DaisyDesign);
-//		ParseSubmitted.printToGrade(Fall2015.GuessingGame1);
+
+
+
 	}
 }
