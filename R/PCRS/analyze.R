2e9d480c9d3246d1708083c19eaa3d48f9413ef1
Thomas Price <twprice@ncsu.edu>
2019-10-30 13:01:21 -0400

    Updated ability to print correct/incorrect PCRS answers

--- R/PCRS/analyze.R
+++ R/PCRS/analyze.R
@@ -22,17 +22,31 @@ loadData <- function() {
   plot(sort(byProblem$nStudents))
   byProblem$problem_id[byProblem$nStudents < 400]
   
-  printSolutions(57, "Fail", 5)
+  printSolutions(42, "Fail", 20)
+  printSolutions(61, "Fail", 5)
+  printSolutions(63, "Fail", 5)
+  
+  printSolutions(35, "Pass", 5)
+  printSolutions(35, "Fail", 5)
 }
 
 printSolutions <- function (id, status, n) {
   p57 <- code.states[code.states$problem_id==id,]
   p57$code <- as.character(p57$code)
   p57$codeW <- str_replace_all(p57$code, "[\\s|\\n]*", "")
-  p57.correct <- table(p57$codeW[p57$status==status])
-  p57.correct <- p57.correct[order(p57.correct, decreasing = T)]
+  p57.correct <- data.frame(codeW=unique(p57$codeW[p57$status==status]))
+  p57.correct$users <- sapply(p57.correct$codeW, function(codeW) length(unique(p57$user_id[p57$codeW == codeW])))
+  p57.correct$total <- sapply(p57.correct$codeW, function(codeW) sum(p57$codeW == codeW))
+  p57.correct <- p57.correct[order(p57.correct$users, decreasing = T),]
+  if (status == "Fail") {
+    p57.correct$indent <- sapply(p57.correct$codeW, function(codeW) mean(p57$status[p57$codeW == codeW] == "Fail") < 1)
+    p57.correct <- p57.correct[p57.correct$indent == F,]
+  }
+  
+  nUsers <- length(unique(p57$user_id))
+  
   for (i in 1:n) {
-    print(paste(i, p57.correct[i], "/", sum(p57.correct), "=", p57.correct[i]/sum(p57.correct), "%"))
-    cat(head(p57$code[p57$codeW == names(p57.correct)[i]], 1))
+    print(paste(i, p57.correct$users[i], "/", nUsers, "=", p57.correct$users[i]*100/nUsers, "%"))
+    cat(head(p57$code[p57$codeW == p57.correct$codeW[i]], 1))
   }
 }
